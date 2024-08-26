06cca452eb52ecb513a39dcd0618740b5a3d43fe
Ruslan Ustsimenka <104977021+rustsimenka@users.noreply.github.com>
2024-08-26 16:35:26 +0300

    объединил в один класс

--- check_wallets.py
+++ check_wallets.py
@@ -9,19 +9,19 @@ class WalletCoins:
         self.wallet = wallet
         self.limit = limit
         self._api_key = api_key
-        self.url = "https://mainnet-aptos-api.moralis.io/wallets/coins"
         self.headers = {"accept": "application/json", "X-API-Key": self._api_key}
         self.params = {"limit": limit, "owner_addresses[0]": self.wallet}
-        self.dict_response = requests.get(url=self.url, headers=self.headers, params=self.params).json()
 
     def check_token(self):
+        url_coins = "https://mainnet-aptos-api.moralis.io/wallets/coins"
+        dict_response = requests.get(url=url_coins, headers=self.headers, params=self.params).json()
 
         print(f'в кошельке {self.wallet}:')
         print()
         sum_usd = 0.0
         dict_tokens = {}
 
-        for coin in self.dict_response['result']:
+        for coin in dict_response['result']:
             address_coin = coin['coin_type']
             amount_coin = float(coin['amount']) / 100000000
             name_coin = coin['coin_type'].split('::')
@@ -45,24 +45,15 @@ class WalletCoins:
         # return f'Итого в кошельке: {sum_usd:3f} USD'
         print(f'Итого в кошельке монет на сумму {total_tokens:3f} USD:')
 
-
-class WalletNfts:
-    def __init__(self, wallet, limit=25):
-        self.wallet = wallet
-        self.limit = limit
-        self._api_key = api_key
-        self.url = "https://mainnet-aptos-api.moralis.io/wallets/nfts"
-        self.headers = {"accept": "application/json", "X-API-Key": self._api_key}
-        self.params = {"limit": limit, "owner_addresses[0]": self.wallet}
-        self.dict_response = requests.get(url=self.url, headers=self.headers, params=self.params).json()
-
     def check_nft(self):
         dict_nfts = {}
+        url_nft = "https://mainnet-aptos-api.moralis.io/wallets/nfts"
+        dict_response = requests.get(url=url_nft, headers=self.headers, params=self.params).json()
 
         print(f'в кошельке {self.wallet}:')
         print()
 
-        for token in self.dict_response['result']:
+        for token in dict_response['result']:
             name_nft = token["collection_name"]
 
             if "collection_name" in token:
@@ -75,3 +66,13 @@ class WalletNfts:
 
         print(f'всего в кошельке {total_nfts} NFT:')
         print(*dict_nfts.items(), sep='\n')
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from check_wallets import WalletCoins, WalletNfts
+from check_wallets import WalletCoins
 from open_txt import my_wallets
 from pprint import pprint
 
@@ -7,9 +7,8 @@ for wallet in my_wallets:
     my_wallet_coins = WalletCoins(wallet)
     my_wallet_coins.check_token()
     print('-----------------')
-    my_wallet_nft = WalletNfts(wallet)
+    my_wallet_coins.check_nft()
 
-    my_wallet_nft.check_nft()
 
 
 
